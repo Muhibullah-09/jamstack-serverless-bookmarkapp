84b3103f363b6b5513ff60faddaf3403a963864a
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-23 02:38:10 +0500

    all functionalities working well data successfully stored in faunadb

---
+++ src/components/card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Card({ url, title }) {
+    return <div className="card">
+        <a href={url} target="_blank" rel="noreferrer" className="url"><b>{url}</b></a>
+        <p className="title">{title}</p>
+    </div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
-import { useQuery,useMutation } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import gql from "graphql-tag";
+import './index.css';
+import Card from '../components/card';
 
 const BookMarksQuery = gql`{
   bookmark{
@@ -19,30 +21,66 @@ const AddBookMarkMutation = gql`
 `
 
 export default function Home() {
- const {loading,error,data} = useQuery(BookMarksQuery)
- const [addBookmark] = useMutation(AddBookMarkMutation)
- let textfield;
- let desc;
- const addBookmarkSubmit = () => {
-  addBookmark({
-    variables: {
-      url: textfield.value,
-      desc: desc.value
-    }
-  })
-   console.log('textfield',textfield.value)
-   console.log('Desc',desc.value)
- }
-  return (
-    <div>
-  <p>{JSON.stringify(data)}</p>
-  <div>
-     <input type="text" placeholder="URL" ref={node => textfield=node}/>
-     <input type="text" placeholder="Description" ref={node => desc=node}/>
-     <button onClick={addBookmarkSubmit}>Add BookMark</button>
-  </div>
-    </div>
+  let textfield;
+  let desc;
 
+  const { loading, error, data } = useQuery(BookMarksQuery)
+  const [addBookmark] = useMutation(AddBookMarkMutation)
+
+  const addBookmarkSubmit = () => {
+    console.log('textfield', textfield.value)
+    console.log('Desc', desc.value)
+    addBookmark({
+      variables: {
+        url: textfield.value,
+        desc: desc.value
+      },
+      refetchQueries: [{ query: BookMarksQuery }]
+    })
+  }
+  if (error)
+    return <h3>{error}</h3>
+
+  if (loading)
+    return <h3>Loading...</h3>
+  return (
+    <div className="container">
+
+      <h2>Add New Bookmark</h2>
+      <label>
+        Enter Bookmark Description: <br />
+        <input type="text" placeholder="Description" ref={node => desc = node} />
+      </label>
+
+      <br />
+      <label>
+        Enter Bookmark Url: <br />
+        <input type="text" placeholder="Enter a valid URL" ref={node => textfield = node} />
+      </label>
+
+      <br />
+      <br />
+      <button onClick={addBookmarkSubmit}>Add Bookmark</button>
+
+      <h2>My Bookmark List</h2>
+
+      <div className="card-container">
+        {data.bookmark.map((bm) => <Card url={bm.url} title={bm.desc} />)}
+      </div>
+    </div>
   )
-  
+
 }
+
+
+    //   {/* <p>{JSON.stringify(data)}</p> */}
+    //   <div>
+    //     <input type="text" placeholder="URL" ref={node => textfield = node} />
+    //     <input type="text" placeholder="Description" ref={node => desc = node} />
+    //     <button onClick={addBookmarkSubmit}>Add BookMark</button>
+    //     <div>
+    //       {data.bookmark.url}
+    //       {data.bookmark.desc}
+    //     </div>
+    //   </div>
+    // </div>
